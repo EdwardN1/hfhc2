3936920e7bb527e3bbba11b4d8c1ac44c02717e7
EdwardN1 <edward@technicks.com>
2019-06-30 14:40:30 +0100

    Job Block Section Responsive

--- parts/blocks/JobBlock.php
+++ parts/blocks/JobBlock.php
@@ -27,17 +27,17 @@ if($double_top_margin) {
 <div class="job-block blog-background<?php echo $padClass; ?>">
     <div class="grid-container white-background pad-top-30 pad-bottom-30 pad-left-0 pad-right-0">
         <div class="grid-x">
-            <div class="cell large-4 medium-4 hide-for-small-only pad-left-30 jobimage">
+            <div class="cell large-4 medium-4 pad-left-30 jobimage">
                 <a class="img" href="<?php echo $page_link; ?>"style="background: url(<?php echo $imageURL; ?>) no-repeat; background-size: cover;">&nbsp;</a>
             </div>
-            <div class="cell large-8 medium-8 small-12 pad-left-30 pad-right-30">
+            <div class="cell large-8 medium-8 small-12 jobdescription">
                 <h2 class="mainheading semi-bold"><?php echo $heading; ?></h2>
                 <h3 class="subheading dark-grey caps semi-bold"><?php echo $sub_heading; ?></h3>
                 <div class="maincontent">
                     <?php echo $content; ?>
                 </div>
                 <div class="joblink">
-                <a href="<?php echo $page_link; ?>" class="round-button"><?php echo $link_description; ?> ></a>
+                <a href="<?php echo $page_link; ?>" class="round-button"><?php echo $link_description; ?></a>
                 </div>
             </div>
         </div>
