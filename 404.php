95d1db95607e933fa23e72acdaac27d51b3ef313
EdwardN1 <GH?841241ed>
2019-07-10 11:53:21 +0100

    404 page

--- 404.php
+++ 404.php
@@ -11,21 +11,14 @@ get_header(); ?>
 
 		<div class="inner-content grid-x grid-margin-x grid-padding-x">
 	
-			<main class="main small-12 medium-8 large-8 cell" role="main">
+			<main class="main small-12 medium-12 large-12 cell" role="main">
 
 				<article class="content-not-found">
 				
-					<header class="article-header">
-						<h1><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
+					<header class="article-header" style="padding-top: 5em; padding-bottom: 5em;">
+						<h1 class="text-center"><?php _e( 'The page you were looking for no longer appears to exist. Please try again', 'jointswp' ); ?></h1>
 					</header> <!-- end article header -->
-			
-					<section class="entry-content">
-						<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
-					</section> <!-- end article section -->
 
-					<section class="search">
-					    <p><?php get_search_form(); ?></p>
-					</section> <!-- end search section -->
 			
 				</article> <!-- end article -->
 	
